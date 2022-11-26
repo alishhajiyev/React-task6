2ebb93fd644ec5398404cdf04cae65a5c68e7278
Alish Hajiyev <alish.haciyev@mail.ru>
2022-11-27 00:55:20 +0400

    Added

--- src/App.jsx
+++ src/App.jsx
@@ -17,71 +17,80 @@ class App extends Component {
   handleInput = (e)=>{
     this.setState({[e.target.name]: e.target.value})
   }
+  submitted=(e)=>{
+    if(this.state.fullname!=="" && this.state.id!=="" && this.state.gender!=="" && this.state.age!=="" && this.state.email!=="" && this.state.phone!=="" && this.state.symptom!==""){
+      alert("Kart qeydiyyatdan keçdi")
+      console.log(`Fullname: ${this.state.fullname}\n Verification ID: ${this.state.id}\n Gender: ${this.state.gender}\n Age: ${this.state.age}\n Email: ${this.state.email}\n Phone number: ${this.state.phone}\n Symptom: ${this.state.symptom}\n Repeat: ${this.state.ischecked}`)
+    }
+    else{
+      alert('Boşluqları doldurun')
+      e.preventDefault()
+    }
+  }
   render(){
     return (
       <div className="App">
-        <label htmlFor="fullname">
-            Full name
-            <br/>
-            <input value={this.state.fullname} onChange={this.handleInput} name='fullname' type='text' />
-        </label>
-        <br/><br/>
+        <form onSubmit={this.submitted}>
+          <label htmlFor="fullname">
+              Full name
+              <br/>
+              <input value={this.state.fullname} onChange={this.handleInput} name='fullname' type='text' />
+          </label>
+          <br/><br/>
 
-        <label htmlFor="id">
-            Verification ID
-            <br/>
-            <input value={this.state.id} onChange={this.handleInput} name='id' type = 'text' />
-        </label>
-        <br/><br/>
+          <label htmlFor="id">
+              Verification ID
+              <br/>
+              <input value={this.state.id} onChange={this.handleInput} name='id' type = 'text' />
+          </label>
+          <br/><br/>
 
-        <label>
-            Gender
-            <br/>
-            <select value={this.state.gender} onChange={this.handleInput} name='gender'>
-                <option value='Male'>Male</option>
-                <option value='Female'>Female</option>
-            </select>
-        </label>
-        <br/><br/>
+          <label>
+              Gender
+              <br/>
+              <select value={this.state.gender} onChange={this.handleInput} name='gender'>
+                  <option value='Male'>Male</option>
+                  <option value='Female'>Female</option>
+              </select>
+          </label>
+          <br/><br/>
 
-        <label>
-            Age
-            <br/>
-            <input htmlFor="age" value={this.state.age} onChange={this.handleInput} name='age' type = 'text' />
-        </label>
-        <br/><br/>
+          <label>
+              Age
+              <br/>
+              <input htmlFor="age" value={this.state.age} onChange={this.handleInput} name='age' type = 'text' />
+          </label>
+          <br/><br/>
 
-        <label>
-            Email
-            <br/>
-            <input htmlFor="email" value={this.state.email} onChange={this.handleInput} name='email' type = 'email' />
-        </label>
-        <br/><br/>
+          <label>
+              Email
+              <br/>
+              <input htmlFor="email" value={this.state.email} onChange={this.handleInput} name='email' type = 'email' />
+          </label>
+          <br/><br/>
 
-        <label>
-            Phone number
-            <br/>
-            <input htmlFor="phone" value={this.state.phone} onChange={this.handleInput} name='phone' type = 'text' />
-        </label>
-        <br/><br/>
+          <label>
+              Phone number
+              <br/>
+              <input htmlFor="phone" value={this.state.phone} onChange={this.handleInput} name='phone' type = 'text' />
+          </label>
+          <br/><br/>
 
-        <label>
-            Symptom
-            <br/>
-        </label>
-        <textarea value={this.state.symptom} onChange={this.handleInput} name='symptom' rows="5" cols="20"></textarea>
-        <br/><br/>
+          <label>
+              Symptom
+              <br/>
+          </label>
+          <textarea value={this.state.symptom} onChange={this.handleInput} name='symptom' rows="5" cols="20"></textarea>
+          <br/><br/>
 
-        <label>
-            Təkrar qəbul
-            <input value={this.state.ischecked} onClick={(e)=>{this.setState({[e.target.id]:!this.state.ischecked})}} name='repeat' type = 'checkbox' />
-        </label>
-        <br/><br/>
+          <label>
+              Təkrar qəbul
+              <input value={this.state.ischecked} onClick={(e)=>{this.setState({[e.target.id]:!this.state.ischecked})}} name='repeat' type = 'checkbox' />
+          </label>
+          <br/><br/>
 
-        <button onClick={()=>{
-          console.log("Kart qeydiyyatdan keçdi")
-          console.log(`Fullname: ${this.state.fullname}\n Verification ID: ${this.state.id}\n Gender: ${this.state.gender}\n Age: ${this.state.age}\n Email: ${this.state.email}\n Phone number: ${this.state.phone}\n Symptom: ${this.state.symptom}\n Repeat: ${this.state.ischecked}`)
-          }}>Отправить</button>
+          <button type="submit">Отправить</button>
+        </form>
       </div>
     )
   }
